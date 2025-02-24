cc825dde43bba435f60ef2ae8a1297eb20f129f6
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-02-25 02:17:30 +0300

    build: add DBUIComponents module

---
+++ DBUIComponents/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "DBUIComponents",
+    defaultLocalization: "ru",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "DBUIComponents",
+            targets: ["DBUIComponents"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.58.0")
+    ],
+    targets: [
+        .target(
+            name: "DBUIComponents"),
+    ]
+)
