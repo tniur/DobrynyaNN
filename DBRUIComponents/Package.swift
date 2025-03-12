76a4272a497596f598fe17eed02fe363c3c2102a
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-03-13 00:34:21 +0300

    refactor: rename DBUIComponents to DBRUIComponents

--- DBRUIComponents/Package.swift
+++ DBRUIComponents/Package.swift
@@ -3,26 +3,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "DBUIComponents",
+    name: "DBRUIComponents",
     defaultLocalization: "ru",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         .library(
-            name: "DBUIComponents",
-            targets: ["DBUIComponents"]),
+            name: "DBRUIComponents",
+            targets: ["DBRUIComponents"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.58.0")
     ],
     targets: [
         .target(
-            name: "DBUIComponents",
+            name: "DBRUIComponents",
             plugins: [
                 .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
             ]
         )
     ]
-    
 )
