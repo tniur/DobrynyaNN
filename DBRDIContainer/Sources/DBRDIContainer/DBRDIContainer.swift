78fd8263a9b67a425701b18d3ce595f08037db29
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-04-22 20:51:42 +0300

    feat: profileService dependency registration

--- DBRDIContainer/Sources/DBRDIContainer/DBRDIContainer.swift
+++ DBRDIContainer/Sources/DBRDIContainer/DBRDIContainer.swift
@@ -1,2 +1,9 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Factory
+import DBRCore
+import DBRNetworking
+
+public extension Container {
+    var profileService: Factory<ProfileService> {
+        Factory(self) { NetworkAPI(client: NetworkClient(baseURL: Constant.baseURL)) }
+    }
+}
