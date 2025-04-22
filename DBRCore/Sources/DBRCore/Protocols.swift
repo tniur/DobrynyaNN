81386f814b0ebf3c1778da6f548ce3021e842696
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-04-22 19:06:38 +0300

    feat: add profile service protocol

--- DBRCore/Sources/DBRCore/Protocols.swift
+++ DBRCore/Sources/DBRCore/Protocols.swift
@@ -1,2 +1,8 @@
 import Foundation
 
+public protocol ProfileService {
+    func fetchPatientInfo() async throws -> PatientInfo
+    func fetchLabResults() async throws -> [LabResult]
+    func fetchDocuments() async throws -> [Document]
+    func fetchConsultations() async throws -> [Consultation]
+}
