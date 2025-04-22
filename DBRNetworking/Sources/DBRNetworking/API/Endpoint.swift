6e1573a41950bf9284ddd1021915c223f24ccf6c
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-04-22 19:20:26 +0300

    feat: add profile endpoints

---
+++ DBRNetworking/Sources/DBRNetworking/API/Endpoint.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum Endpoint {
+    static let patientInfo = "getPatientInfo"
+    static let labResults = "getPatientLabResults"
+    static let documents = "getPatientDocuments"
+    static let consultations = "getPatientConsultations"
+}
