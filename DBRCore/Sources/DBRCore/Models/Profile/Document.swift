af31846e0f04398b32a596df86f4b4152e203b84
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-04-22 19:01:08 +0300

    feat: add profile models

---
+++ DBRCore/Sources/DBRCore/Models/Profile/Document.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public struct Document: Sendable {
+    public let id: Int
+    public let title: String
+    public let doctorId: Int
+    public let symptoms: String
+    public let diagnosis: String
+    public let conclusion: String
+    public let recommendations: String
+    public let isTemp: Bool
+    public let dateReturn: String
+    
+    public init(id: Int,
+                title: String,
+                doctorId: Int,
+                symptoms: String,
+                diagnosis: String,
+                conclusion: String,
+                recommendations: String,
+                isTemp: Bool,
+                dateReturn: String
+    ) {
+        self.id = id
+        self.title = title
+        self.doctorId = doctorId
+        self.symptoms = symptoms
+        self.diagnosis = diagnosis
+        self.conclusion = conclusion
+        self.recommendations = recommendations
+        self.isTemp = isTemp
+        self.dateReturn = dateReturn
+    }
+}
---
+++ DBRCore/Sources/DBRCore/Models/Profile/PatientInfo.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+public struct PatientInfo: Sendable {
+    public let lastName: String
+    public let firstName: String
+    public let thirdName: String
+    public let birthDate: String
+    public let age: Int
+    public let gender: String
+    public let mobile: String
+    public let email: String
+    
+    public init(
+        lastName: String,
+        firstName: String,
+        thirdName: String,
+        birthDate: String,
+        age: Int,
+        gender: String,
+        mobile: String,
+        email: String
+    ) {
+        self.lastName = lastName
+        self.firstName = firstName
+        self.thirdName = thirdName
+        self.birthDate = birthDate
+        self.age = age
+        self.gender = gender
+        self.mobile = mobile
+        self.email = email
+    }
+}
