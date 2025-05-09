abb455e2b2ed32de62e404d5bc515c95477dc497
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-05-09 23:33:48 +0300

    refactor: rename network to api service

--- DBRDIContainer/Sources/DBRDIContainer/DBRDIContainer.swift
+++ DBRDIContainer/Sources/DBRDIContainer/DBRDIContainer.swift
@@ -5,25 +5,25 @@ import DBRNetworking
 
 public extension Container {
     var profileService: Factory<ProfileService> {
-        Factory(self) { NetworkService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
+        Factory(self) { APIService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
     }
 }
 
 public extension Container {
     var clinicService: Factory<ClinicService> {
-        Factory(self) { NetworkService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
+        Factory(self) { APIService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
     }
 }
 
 public extension Container {
     var doctorService: Factory<DoctorService> {
-        Factory(self) { NetworkService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
+        Factory(self) { APIService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
     }
 }
 
 public extension Container {
     var appointmentsService: Factory<AppointmentsService> {
-        Factory(self) { NetworkService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
+        Factory(self) { APIService(client: NetworkClient(baseURL: Constant.baseURL), accessTokenKey: Constant.mockPatientKey) }
     }
 }
 
