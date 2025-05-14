71ef60c1e3ff1d9ca20089c6c7204e769f4bae84
Pavel Bobkov <pavel.bbkv1@gmail.com>
2025-05-14 16:24:20 +0300

    refactor: append DBR prefix for types in DBRDIContainer

--- DBRDIContainer/Sources/DBRDIContainer/DBRDIContainer.swift
+++ DBRDIContainer/Sources/DBRDIContainer/DBRDIContainer.swift
@@ -6,47 +6,47 @@ import DBRNetworking
 // MARK: - Token Provider
 
 public extension Container {
-    var tokenProvider: Factory<TokenProvider> {
-        Factory(self) { AccessTokenProvider() }
+    var tokenProvider: Factory<DBRTokenProvider> {
+        Factory(self) { DBRAccessTokenProvider() }
     }
 }
 
 // MARK: - PDF Manager
 
 public extension Container {
-    var pdfManager: Factory<PDFManager> {
-        Factory(self) { DocumentManager() }
+    var pdfManager: Factory<DBRPDFManager> {
+        Factory(self) { DBRDocumentManager() }
     }
 }
 
 // MARK: - Services
 
 public extension Container {
-    var profileService: Factory<ProfileService> {
-        return Factory(self) { APIService(client: NetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
+    var profileService: Factory<DBRProfileService> {
+        return Factory(self) { DBRAPIService(client: DBRNetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
     }
 }
 
 public extension Container {
-    var clinicService: Factory<ClinicService> {
-        Factory(self) { APIService(client: NetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
+    var clinicService: Factory<DBRClinicService> {
+        Factory(self) { DBRAPIService(client: DBRNetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
     }
 }
 
 public extension Container {
-    var doctorService: Factory<DoctorService> {
-        Factory(self) { APIService(client: NetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
+    var doctorService: Factory<DBRDoctorService> {
+        Factory(self) { DBRAPIService(client: DBRNetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
     }
 }
 
 public extension Container {
-    var appointmentsService: Factory<AppointmentsService> {
-        Factory(self) { APIService(client: NetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
+    var appointmentsService: Factory<DBRAppointmentsService> {
+        Factory(self) { DBRAPIService(client: DBRNetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
     }
 }
 
 public extension Container {
-    var authService: Factory<AuthService> {
-        Factory(self) { APIService(client: NetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
+    var authService: Factory<DBRAuthService> {
+        Factory(self) { DBRAPIService(client: DBRNetworkClient(), tokenProvider: Container.shared.tokenProvider()) }
     }
 }
