afe4656e2e700b2c27c32725c31e8f8411305c33
Pavel Bobkov <pavel.bbkv1@gmail.com>
2025-05-18 15:49:34 +0300

    refactor: rename auth mappers

--- DBRAPIService/Sources/DBRAPIService/APIService/Services/DBRAuthService.swift
+++ DBRAPIService/Sources/DBRAPIService/APIService/Services/DBRAuthService.swift
@@ -3,19 +3,19 @@ import DBRNetworking
 import DBRCore
 
 extension DBRAPIService: DBRAuthService {
-    public func requestCode(login: String, password: String) async throws -> DBRRequestCodeResult {
+    public func requestCode(login: String, password: String) async throws -> DBRAuthRequestCodeResult {
         do {
-            let data = try await client.send(DBRResources.requestCode(login: login, password: password).post).data
-            return DBRRequestCodeMapper.map(data)
+            let data = try await client.send(DBRResources.authRequestCode(login: login, password: password).post).data
+            return DBRAuthRequestCodeMapper.map(data)
         } catch {
             throw handle(error)
         }
     }
 
-    public func confirmCode(login: String, code: String) async throws -> DBRConfirmationResult {
+    public func confirmCode(login: String, code: String) async throws -> DBRAuthConfirmationResult {
         do {
-            let data = try await client.send(DBRResources.confirmCode(login: login, code: code).post).data
-            return DBRConfirmCodeMapper.map(data)
+            let data = try await client.send(DBRResources.authConfirmCode(login: login, code: code).post).data
+            return DBRAuthConfirmCodeMapper.map(data)
         } catch {
             throw handle(error)
         }
