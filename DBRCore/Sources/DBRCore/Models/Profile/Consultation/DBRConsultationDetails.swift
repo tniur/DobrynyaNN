4772a063ee11deda29de028467bb25bf56b150c0
Pavel Bobkov <pavel.bbkv1@gmail.com>
2025-05-23 10:34:19 +0300

    feat: add doctor name for consultation details

--- DBRCore/Sources/DBRCore/Models/Profile/Consultation/DBRConsultationDetails.swift
+++ DBRCore/Sources/DBRCore/Models/Profile/Consultation/DBRConsultationDetails.swift
@@ -4,13 +4,15 @@ public struct DBRConsultationDetails: Sendable, Identifiable {
     public let id: Int
     public let title: String
     public let doctorId: Int
+    public let doctorName: String
     public let status: DBRConsultationStatus
     public let description: String
 
-    public init(id: Int, title: String, doctorId: Int, status: DBRConsultationStatus, description: String) {
+    public init(id: Int, title: String, doctorId: Int, doctorName: String, status: DBRConsultationStatus, description: String) {
         self.id = id
         self.title = title
         self.doctorId = doctorId
+        self.doctorName = doctorName
         self.status = status
         self.description = description
     }
