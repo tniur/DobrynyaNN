baf70d5191ef831e24cf8651e4983bbf58a2cb8f
Zhuravleva Anastasia <nas20040908@gmail.com>
2025-05-23 10:59:54 +0300

    feat: add marks and emptyView

--- DBR/DobrynyaNN/Visits/DBRVisitsView.swift
+++ DBR/DobrynyaNN/Visits/DBRVisitsView.swift
@@ -10,31 +10,67 @@ import DBRUIComponents
 
 struct DBRVisitsView: View {
     
+    // MARK: - Properties
+
     @StateObject private var viewModel: DBRVisitsViewModel
     
+    // MARK: - Body
+
     var body: some View {
+        contentView
+            .navigationTitle("Мои визиты")
+    }
+    
+    // MARK: - Subviews
+
+    private var contentView: some View {
         VStack {
             DBRVisitsToggle(sections: ["Будущие", "Прошедшие"])
             
-            ScrollView {
-                LazyVStack {
-                    ForEach(viewModel.visits) { visit in
-                        DBRAnalisysView(
-                            title: visit.serviceTitle,
-                            adress: visit.clinicAddress,
-                            doctorName: visit.doctorName,
-                            createdDate: visit.createdDate
-                        )
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.top, 32.0)
+            if viewModel.visits.isEmpty {
+                Spacer()
+                
+                emptyView
+                    .frame(width: 226.0, alignment: .center)
+                    .padding(.horizontal)
+                
+                Spacer()
+            } else {
+                scrollView
             }
-            .scrollIndicators(.hidden)
         }
+        .padding(.top, 4.0)
     }
     
+    private var scrollView: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(viewModel.visits) { visit in
+                    DBRAnalisysView(
+                        title: visit.serviceTitle,
+                        adress: visit.clinicAddress,
+                        doctorName: visit.doctorName,
+                        createdDate: visit.createdDate
+                    )
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 32.0)
+        }
+        .scrollIndicators(.hidden)
+    }
+    
+    private var emptyView: some View {
+        Text("У вас пока нет предстоящих визитов. Как только они появятся, вы сможете просмотреть их здесь.")
+            .font(DBRFont.R16)
+            .foregroundStyle(DBRColor.base4.swiftUIColor)
+            .multilineTextAlignment(.center)
+    }
+    
+    // MARK: - Initializer
+
     init(viewModel: DBRVisitsViewModel) {
         self._viewModel = StateObject(wrappedValue: viewModel)
     }
 }
+
--- DBR/DobrynyaNN/Visits/DBRVisitsViewModel.swift
+++ DBR/DobrynyaNN/Visits/DBRVisitsViewModel.swift
@@ -17,7 +17,7 @@ final class DBRVisitsViewModel: ObservableObject {
     @Injected(\.appointmentsService) private var appointmentsService: DBRAppointmentsService
 
     @Published var selectedIndex: Int = 0
-    @Published var visits: [DBRAppointment] = [.init(id: 1, date: "434", time: "3434", timeStart: "35435", timeEnd: "35", clinicId: 4, clinicAddress: "3535", doctorId: 4, doctorName: "fghjk", serviceId: 4, serviceTitle: "gbdgfds", createdDate: "bds", status: .upcoming)]
+    @Published var visits: [DBRAppointment] = []
     
     private var screenNavigator: ScreenNavigator
     private let screens: DBRVisitsScreens
