589e33cbc31a0ddd75f759ce5faf331d071c6102
Pavel Bobkov <pavel.bbkv1@gmail.com>
2025-05-26 22:49:01 +0300

    feat: update patientService, add new mappers

---
+++ DBRAPIService/Sources/DBRAPIService/APIService/Mappers/Models/DBRUpdatePatientInfoResultMapper.swift
@@ -0,0 +1,9 @@
+import Foundation
+import DBRNetworking
+import DBRCore
+
+struct DBRUpdatePatientInfoResultMapper {
+    static func map(_ dto: DBRUpdatePatientInfoResultDTO) -> DBRUpdatePatientInfoResult {
+        DBRUpdatePatientInfoResult()
+    }
+}
